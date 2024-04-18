66aa83839d9ee2e611c80186350080adea379f68
sandy <sandeshjadhav2002@gmail.com>
2024-04-18 20:54:29 +0530

    docker image setup

---
+++ main.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+const PORT = process.env.PORT || 8000
+
+
+app.get('/',(req,res) => {
+  return res.json({message:"Hey, i am node js in container"})
+})
+
+app.listen(PORT,() => {
+    console.log(`Server starting on PORT: ${PORT}`);
+})
